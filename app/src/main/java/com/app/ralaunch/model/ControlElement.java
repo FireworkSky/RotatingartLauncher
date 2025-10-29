3ff37248610af881403bc86d9551b6e4fdecd942
Fireworks <1773931858@qq.com>
2025-10-29 10:45:26 +0800

    Migrating from hostfxr runapp to coreclr
    net10 coreclr does not have libhostfxr.so, so for now only coreclr can be used

--- app/src/main/java/com/app/ralaunch/model/ControlElement.java
+++ app/src/main/java/com/app/ralaunch/model/ControlElement.java
@@ -28,7 +28,7 @@ public class ControlElement {
     private String name;
     private String displayText;  // 显示文本
     private String iconPath;     // 图标路径
-    
+
     // 位置和大小（相对坐标 0-1）
     private float x;
     private float y;
@@ -36,19 +36,19 @@ public class ControlElement {
     private float height;
     private float rotation;      // 旋转角度
     private float cornerRadius;  // 圆角半径
-    
+
     // 键位绑定
     private int keyCode;
     private int keyCode2;        // 组合键
     private String macroSequence; // 宏序列
-    
+
     // 行为设置
     private boolean toggle;
     private boolean passthrough;
     private boolean swipeClick;
     private boolean repeatEnabled; // 连按
     private int repeatDelay;      // 连按延迟ms
-    
+
     // 外观设置
     private int backgroundColor;
     private int pressedColor;     // 按下时颜色
@@ -57,17 +57,17 @@ public class ControlElement {
     private float borderWidth;
     private float opacity;
     private float textSize;
-    
+
     // 显示设置
     private Visibility visibility;
     private boolean dynamicPosition; // 动态跟随手指
     private boolean snapToEdge;      // 吸附到边缘
-    
+
     // 摇杆特有属性
     private float deadzone;          // 死区
     private float sensitivity;       // 灵敏度
     private boolean lockDirection;   // 锁定方向（4/8方向）
-    
+
     // 触摸板特有属性
     private float scrollSensitivity;
     private boolean invertX;
@@ -78,13 +78,13 @@ public class ControlElement {
         this.type = type;
         this.name = name;
         this.displayText = name;
-        
+
         // 默认值
         this.width = 80;
         this.height = 80;
         this.rotation = 0;
         this.cornerRadius = 10;
-        
+
         this.backgroundColor = Color.argb(128, 100, 100, 100);
         this.pressedColor = Color.argb(180, 150, 150, 150);
         this.borderColor = Color.WHITE;
@@ -92,10 +92,10 @@ public class ControlElement {
         this.borderWidth = 2.0f;
         this.opacity = 0.7f;
         this.textSize = 16f;
-        
+
         this.visibility = Visibility.ALWAYS;
         this.repeatDelay = 100;
-        
+
         // 摇杆默认值
         this.deadzone = 0.1f;
         this.sensitivity = 1.0f;
@@ -107,13 +107,13 @@ public class ControlElement {
         String id = json.getString("id");
         ElementType type = ElementType.valueOf(json.getString("type"));
         String name = json.getString("name");
-        
+
         ControlElement element = new ControlElement(id, type, name);
-        
+
         // 基本属性
         if (json.has("displayText")) element.displayText = json.getString("displayText");
         if (json.has("iconPath")) element.iconPath = json.getString("iconPath");
-        
+
         // 位置和大小
         if (json.has("x")) element.x = (float) json.getDouble("x");
         if (json.has("y")) element.y = (float) json.getDouble("y");
@@ -121,19 +121,19 @@ public class ControlElement {
         if (json.has("height")) element.height = (float) json.getDouble("height");
         if (json.has("rotation")) element.rotation = (float) json.getDouble("rotation");
         if (json.has("cornerRadius")) element.cornerRadius = (float) json.getDouble("cornerRadius");
-        
+
         // 键位绑定
         if (json.has("keyCode")) element.keyCode = json.getInt("keyCode");
         if (json.has("keyCode2")) element.keyCode2 = json.getInt("keyCode2");
         if (json.has("macroSequence")) element.macroSequence = json.getString("macroSequence");
-        
+
         // 行为设置
         if (json.has("toggle")) element.toggle = json.getBoolean("toggle");
         if (json.has("passthrough")) element.passthrough = json.getBoolean("passthrough");
         if (json.has("swipeClick")) element.swipeClick = json.getBoolean("swipeClick");
         if (json.has("repeatEnabled")) element.repeatEnabled = json.getBoolean("repeatEnabled");
         if (json.has("repeatDelay")) element.repeatDelay = json.getInt("repeatDelay");
-        
+
         // 外观设置
         if (json.has("backgroundColor")) element.backgroundColor = json.getInt("backgroundColor");
         if (json.has("pressedColor")) element.pressedColor = json.getInt("pressedColor");
@@ -142,35 +142,35 @@ public class ControlElement {
         if (json.has("borderWidth")) element.borderWidth = (float) json.getDouble("borderWidth");
         if (json.has("opacity")) element.opacity = (float) json.getDouble("opacity");
         if (json.has("textSize")) element.textSize = (float) json.getDouble("textSize");
-        
+
         // 显示设置
         if (json.has("visibility")) element.visibility = Visibility.valueOf(json.getString("visibility"));
         if (json.has("dynamicPosition")) element.dynamicPosition = json.getBoolean("dynamicPosition");
         if (json.has("snapToEdge")) element.snapToEdge = json.getBoolean("snapToEdge");
-        
+
         // 摇杆特有
         if (json.has("deadzone")) element.deadzone = (float) json.getDouble("deadzone");
         if (json.has("sensitivity")) element.sensitivity = (float) json.getDouble("sensitivity");
         if (json.has("lockDirection")) element.lockDirection = json.getBoolean("lockDirection");
-        
+
         // 触摸板特有
         if (json.has("scrollSensitivity")) element.scrollSensitivity = (float) json.getDouble("scrollSensitivity");
         if (json.has("invertX")) element.invertX = json.getBoolean("invertX");
         if (json.has("invertY")) element.invertY = json.getBoolean("invertY");
-        
+
         return element;
     }
 
     // 转换为 JSON
     public JSONObject toJSON() throws JSONException {
         JSONObject json = new JSONObject();
-        
+
         json.put("id", id);
         json.put("type", type.name());
         json.put("name", name);
         json.put("displayText", displayText);
         if (iconPath != null) json.put("iconPath", iconPath);
-        
+
         // 位置和大小
         json.put("x", x);
         json.put("y", y);
@@ -178,19 +178,19 @@ public class ControlElement {
         json.put("height", height);
         json.put("rotation", rotation);
         json.put("cornerRadius", cornerRadius);
-        
+
         // 键位绑定
         json.put("keyCode", keyCode);
         json.put("keyCode2", keyCode2);
         if (macroSequence != null) json.put("macroSequence", macroSequence);
-        
+
         // 行为设置
         json.put("toggle", toggle);
         json.put("passthrough", passthrough);
         json.put("swipeClick", swipeClick);
         json.put("repeatEnabled", repeatEnabled);
         json.put("repeatDelay", repeatDelay);
-        
+
         // 外观设置
         json.put("backgroundColor", backgroundColor);
         json.put("pressedColor", pressedColor);
@@ -199,22 +199,22 @@ public class ControlElement {
         json.put("borderWidth", borderWidth);
         json.put("opacity", opacity);
         json.put("textSize", textSize);
-        
+
         // 显示设置
         json.put("visibility", visibility.name());
         json.put("dynamicPosition", dynamicPosition);
         json.put("snapToEdge", snapToEdge);
-        
+
         // 摇杆特有
         json.put("deadzone", deadzone);
         json.put("sensitivity", sensitivity);
         json.put("lockDirection", lockDirection);
-        
+
         // 触摸板特有
         json.put("scrollSensitivity", scrollSensitivity);
         json.put("invertX", invertX);
         json.put("invertY", invertY);
-        
+
         return json;
     }
 
